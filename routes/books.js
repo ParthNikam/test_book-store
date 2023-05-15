aa061d1222b9e19b7260cffb34718241166f9571
parth108 <108parthnikam@gmail.com>
2023-05-15 06:10:25 +0530

    added filepond for file upload

--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,10 @@
 import Router from 'express';
-import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
-import Book, { coverImageBasePath } from '../models/book.js';
+import Book from '../models/book.js';
 import Author from '../models/author.js';
 
 
 const router = Router();
-const uploadPath = path.join('public', coverImageBasePath)
-const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
-const upload = multer({
-  dest: uploadPath,
-  fileFilter: (req, file, callback) => {
-    callback(null, imageMimeTypes.includes(file.mimetype))
-  }
-})
+
 
 
 // All Books Route
@@ -49,35 +39,36 @@ router.get('/new', async (req, res) => {
 
 
 // Create Book Route
-router.post('/', upload.single('cover'), async (req, res) => {
-  const fileName = req.file != null ? req.file.filename : null
+router.post('/', async (req, res) => {
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
-    coverImageName: fileName,
     description: req.body.description
-  })
+  });
+
+  saveCover(book, req.body.cover);
 
   try {
     const newBook = await book.save()
     // res.redirect(`books/${newBook.id}`)
     res.redirect(`books`)
   } catch {
-    if (book.coverImageName != null) {
-      removeBookCover(book.coverImageName)
-    }
     renderNewPage(res, book, true)
   }
 })
 
 
-function removeBookCover(fileName) {
-  fs.unlink(path.join(uploadPath, fileName), err => {
-    if (err) console.error(err)
-  })
-}
+function saveCover(book, coverEncoded) {
+  if (coverEncoded == null) return ;
+  const cover = JSON.parse(coverEncoded);
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    book.coverImage = new Buffer.from(cover.data, "base64");
+    book.coverImageType = cover.type;
+  }
+} 
+
 
 
 async function renderNewPage(res, book, hasError = false) {
--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 
 import mongoose from 'mongoose';
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
-db.once('open', () => console.log('Connected to Mongoose'));
+
 
 
 app.use('/', indexRouter);
